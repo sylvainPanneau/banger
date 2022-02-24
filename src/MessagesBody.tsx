849ecce1bffdda037af4f1407d1a2b2471865b79
Sylvain Panneau <fkorangina@gmail.com>
2022-02-24 22:24:42 +0100

    loading screen for messages

--- src/MessagesBody.tsx
+++ src/MessagesBody.tsx
@@ -2,9 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { useRef } from 'react';
 import { supabase } from './setupSupabase';
 import { v4 as uuidv4 } from 'uuid';
+import { ProgressBar } from 'react-bootstrap';
+import MessagesLoading from './MessagesLoading';
 
 export default function MessagesBody({ messages }: { messages: Array<any> }) {
   const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    console.log("local storage : ", localStorage.getItem('messages'));
+  }, []);
 
   useEffect(() => {
     if (containerRef.current) {
@@ -13,22 +19,25 @@ export default function MessagesBody({ messages }: { messages: Array<any> }) {
   }, [messages]);
 
   return (
-    <div className="messages-body" ref={containerRef}>
-      {messages.map(message => {
-        return message.origin === supabase.auth.user()?.id ? (
-          <div key={uuidv4()} className="message-wrapper-me">
-            <div className="message-me">
-              <p className="message-text">{message.message}</p>
-            </div>
-          </div>
-        ) : (
-          <div key={uuidv4()} className="message-wrapper-other">
-            <div className="message-other">
-              <p className="message-text">{message.message}</p>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      {localStorage.getItem('messages') === null ? (<MessagesLoading />) :
+        (<div className="messages-body" ref={containerRef}>
+          {messages.map(message => {
+            return message.origin === supabase.auth.user()?.id ? (
+              <div key={uuidv4()} className="message-wrapper-me">
+                <div className="message-me">
+                  <p className="message-text">{message.message}</p>
+                </div>
+              </div>
+            ) : (
+              <div key={uuidv4()} className="message-wrapper-other">
+                <div className="message-other">
+                  <p className="message-text">{message.message}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>)}
+    </>
   );
 }
